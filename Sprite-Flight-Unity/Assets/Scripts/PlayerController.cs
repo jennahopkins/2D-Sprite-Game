58f43e88ad05abf9fc934d66a73e931751fa74b0
Jenna Hopkins <jhopkins2@willamette.edu>
2025-09-11 11:00:45 -0700

    completed fourth tutorial

---
+++ Sprite-Flight-Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float thrustForce = 1f;
+    public float maxSpeed = 5f;
+    public GameObject boosterFlame;
+    Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mouse.current.leftButton.isPressed)
+        {
+            // Calculate mouse direction
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
+            Vector2 direction = (mousePos - transform.position).normalized;
+
+            // Move player in direction of mouse
+            transform.up = direction;
+            rb.AddForce(direction * thrustForce);
+
+            // Limit max speed if mouse is held down
+            if (rb.linearVelocity.magnitude > maxSpeed)
+            {
+                rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+            }
+        }
+
+        // Toggle booster flame visibility based on mouse button state
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            boosterFlame.SetActive(true);
+        }
+        else if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            boosterFlame.SetActive(false);
+        }
+    }
+    
+    // Destroy the player on collision with an obstacle
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
