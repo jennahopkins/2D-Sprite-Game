93b89aea14fe7c7cd6cbc54c68a7c8ca31568cfa
Jenna Hopkins <jhopkins2@willamette.edu>
2025-09-10 22:09:51 -0700

    third tutorial completed

---
+++ Sprite-Flight-Unity/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public float minSize = 0.5f;
+    public float maxSize = 1.5f;
+    public float minSpeed = 50f;
+    public float maxSpeed = 150f;
+    public float maxSpinSpeed = 10f;
+
+    Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        float randomSize = Random.Range(minSize, maxSize);
+        transform.localScale = new Vector3(randomSize, randomSize, 1);
+
+        float randomSpeed = Random.Range(minSpeed, maxSpeed) / randomSize;
+        Vector2 randomDirection = Random.insideUnitCircle;
+
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(randomDirection * randomSpeed);
+
+        float randomTorque = Random.Range(-maxSpinSpeed, maxSpinSpeed);
+        rb.AddTorque(randomTorque);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
